42937d3487abc6468934ad018d1017475402d703
oluwadaprof <adeekotobiloba8@gmail.com>
2022-01-15 11:32:21 +0100

    project-setup

---
+++ healthcare/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+import Logo from './components/Logo';
+
+function App() {
+  return (
+    <div className="App">
+      <Logo />
+    </div>
+  );
+}
+
+export default App;
---
+++ healthcare/src/components/Logo.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+function Logo() {
+    return (
+        <>
+            <div className='logo'>
+                T
+            </div>
+            <p className='logo-text'>Trafalgar</p>
+        </>
+        
+    )
+}
+
+export default Logo;
